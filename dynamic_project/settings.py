ebf68debf74f2c1c3dcb6c7cf46a512914c0e3c8
bharathjinka09 <bharathjinka09@gmail.com>
2022-01-20 18:16:43 +0530

    static root

--- dynamic_project/settings.py
+++ dynamic_project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'STATIC_ROOT
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
